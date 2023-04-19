9530e69281f243743486ff961a3a31f0c73fa055
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2023-04-19 18:25:01 +0200

    Added a Promo because why not

--- src/Core.cpp
+++ src/Core.cpp
@@ -4,13 +4,25 @@
 
 namespace Console {
 
+	void DisplayLicense();
+
 	void Initialize() {
 
-		std::cout << "Initialized the Console App Template!" << std::endl;
+		DisplayLicense();
 
 	}
 
 	void Log(const std::string& message) { std::cout << message << std::endl; }
 	void Log(const char* message) { std::cout << message << std::endl; }
+
+	void DisplayLicense() {
+
+		std::cout << "----------------------------------------------------------------------------------------" << std::endl;
+		std::cout << "-----This App has been made using the Console App Tempalte developed by Chris Husky-----" << std::endl;
+		std::cout << "-------------------------https://github.com/OfficialChrisHusky--------------------------" << std::endl;
+		std::cout << "----------------------------------------------------------------------------------------" << std::endl;
+		std::cout << std::endl;
+
+	}
 
 }
